20a9e89e52f9613b65a30a12c9aa06b433a2a4f9
Mahmut Alperen Ünal <mahmutalperenunal@gmail.com>
2025-06-10 18:37:22 +0300

    fix: integrate AdMob banner

--- app/src/main/java/com/mahmutalperenunal/kodex/KodexApp.kt
+++ app/src/main/java/com/mahmutalperenunal/kodex/KodexApp.kt
@@ -1,7 +1,10 @@
 package com.mahmutalperenunal.kodex
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -9,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import com.mahmutalperenunal.kodex.ui.screens.GeneratorScreen
 import com.mahmutalperenunal.kodex.ui.screens.HistoryScreen
@@ -18,6 +22,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.mahmutalperenunal.kodex.ui.components.AdMobBanner
 
 @Composable
 fun KodexApp() {
@@ -35,30 +40,40 @@ fun KodexApp() {
     Box(modifier = Modifier.fillMaxSize()) {
         Scaffold(
             bottomBar = {
-                NavigationBar {
-                    bottomNavItems.forEach { item ->
-                        NavigationBarItem(
-                            icon = {
-                                when (val icon = item.icon) {
-                                    is BottomNavIcon.Vector -> Icon(imageVector = icon.icon, contentDescription = item.label)
-                                    is BottomNavIcon.Drawable -> Icon(
-                                        painter = painterResource(id = icon.resId),
-                                        contentDescription = item.label
-                                    )
-                                }
-                            },
-                            label = { Text(item.label) },
-                            selected = currentRoute == item.route,
-                            onClick = {
-                                navController.navigate(item.route) {
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    AdMobBanner(
+                        adUnitId = "ca-app-pub-xxxxxxxxxxxxxxxx/zzzzzzzzzz",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    )
+                    NavigationBar {
+                        bottomNavItems.forEach { item ->
+                            NavigationBarItem(
+                                icon = {
+                                    when (val icon = item.icon) {
+                                        is BottomNavIcon.Vector -> Icon(imageVector = icon.icon, contentDescription = item.label)
+                                        is BottomNavIcon.Drawable -> Icon(
+                                            painter = painterResource(id = icon.resId),
+                                            contentDescription = item.label
+                                        )
                                     }
-                                    launchSingleTop = true
-                                    restoreState = true
+                                },
+                                label = { Text(item.label) },
+                                selected = currentRoute == item.route,
+                                onClick = {
+                                    navController.navigate(item.route) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
                                 }
-                            }
-                        )
+                            )
+                        }
                     }
                 }
             }
@@ -76,16 +91,6 @@ fun KodexApp() {
                     composable("generator") { GeneratorScreen(navController) }
                     composable("history") { HistoryScreen(navController) }
                 }
-
-                /*if (currentRoute != "scanner") {
-                    AdMobBanner(
-                        adUnitId = "ca-app-pub-xxxxxxxxxxxxxxxx/zzzzzzzzzz",
-                        modifier = Modifier
-                            .align(Alignment.BottomCenter)
-                            .fillMaxWidth()
-                            .height(50.dp)
-                    )
-                }*/
             }
         }
     }
