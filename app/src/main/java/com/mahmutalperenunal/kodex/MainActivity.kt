ac2646b05bad477a94da21c1c107b493483af2e4
Mahmut Alperen Ünal <mahmutalperenunal@gmail.com>
2025-06-13 10:02:58 +0300

    fix: prevent NullPointerException on Android 15 by safely accessing insetsController

--- app/src/main/java/com/mahmutalperenunal/kodex/MainActivity.kt
+++ app/src/main/java/com/mahmutalperenunal/kodex/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.mahmutalperenunal.kodex
 
 import android.content.Context
-import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
@@ -17,7 +16,6 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mahmutalperenunal.kodex.ui.theme.KodexTheme
 import com.mahmutalperenunal.kodex.utils.VersionChecker
 import kotlinx.coroutines.launch
-import androidx.core.net.toUri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,16 +32,17 @@ class MainActivity : ComponentActivity() {
             else -> resources.configuration.uiMode and 0x30 == 0x20 // UI_MODE_NIGHT_YES
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val controller = window?.insetsController
-            controller?.setSystemBarsAppearance(
-                if (isDarkTheme) 0 else WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
-                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-            )
-        } else {
-            window?.decorView?.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN) or
-                        if (!isDarkTheme) View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR else 0
+        window.decorView.post {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                window.insetsController?.setSystemBarsAppearance(
+                    if (isDarkTheme) 0 else WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                )
+            } else {
+                window.decorView.systemUiVisibility =
+                    (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN) or
+                            if (!isDarkTheme) View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR else 0
+            }
         }
 
         lifecycleScope.launch {
